a07f7fecf3ffac0cc64b386e131b4bea800cbc64
sungbin <21700613@handong.edu>
2018-12-27 22:22:37 +0900

    SB solved A+B

---
+++ problems/알고리즘 기초/chapter01/A+B/임성빈.java
@@ -0,0 +1,8 @@
+class Main {
+	public static void main(String[] args) {
+		java.util.Scanner in = new java.util.Scanner(System.in);
+		int a = in.nextInt();
+		int b = in.nextInt();
+		System.out.println(a+b);
+	}
+}
