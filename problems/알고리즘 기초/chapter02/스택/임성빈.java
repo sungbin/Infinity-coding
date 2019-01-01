e1ddb2373eaaa1c3ae151b375731cc05cba70c0f
sungbin <21700613@handong.edu>
2019-01-02 01:12:41 +0900

    sb solved 스택

--- problems/알고리즘 기초/chapter02/스택/임성빈.java
+++ problems/알고리즘 기초/chapter02/스택/임성빈.java
@@ -1,8 +1,37 @@
 class Main {
 	public static void main(String[] args) {
 		java.util.Scanner in = new java.util.Scanner(System.in);
-		int a = in.nextInt();
-		int b = in.nextInt();
-		System.out.println(a+b);
+		java.util.Stack<Integer> stack = new java.util.Stack<Integer>(); 
+		
+		int T = in.nextInt();
+		
+		for(int i=1; i<=T; i++) {
+			String word = in.next();
+			switch(word) {
+			case "push":
+				int n = in.nextInt();
+				stack.push(n);
+				break;
+			case "pop":
+				try {
+					System.out.println(stack.pop());
+				} catch(Exception e) {System.out.println("-1");}
+				break;
+			case "size":
+				System.out.println(stack.size());
+				break;
+			case "empty":
+				if(stack.empty())
+					System.out.println("1");
+				else
+					System.out.println("0");
+				break;
+			case "top":
+				try {
+					System.out.println(stack.peek());
+				} catch(Exception e) {System.out.println("-1");}
+				break;
+			}
+		}
 	}
 }
